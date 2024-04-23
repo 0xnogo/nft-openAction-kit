b42f56b1c6f550678f85b9f7550893919bbdea2a
defispartan <defispartan@gmail.com>
2024-04-23 18:33:46 -0500

    feat: safetransferfrom contract (#25)
    * feat: add aggregated bridge fee to action response
    
    * chore: package version
    
    * feat: contract version with safetransferfrom

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,20 +3,10 @@ import { ChainConfig } from "../types";
 
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
-export const network: string = "polygon"; // options: 'polygon', 'mumbai'
-
-export const CHAIN_CONFIG: ChainConfig =
-  network === "polygon"
-    ? {
-        lensHubProxyAddress: "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d",
-        decentOpenActionContractAddress:
-          "0x2357a69BAF2584cf25A3B9bAA2c7eE6063cb8B40",
-      }
-    : {
-        lensHubProxyAddress: "0x4fbffF20302F3326B20052ab9C217C44F6480900",
-        decentOpenActionContractAddress:
-          "0xe310b5Ed0B3c19B1F0852Ce985a4C38BAE738FDb",
-      };
+export const CHAIN_CONFIG: ChainConfig = {
+  lensHubProxyAddress: "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d",
+  decentOpenActionContractAddress: "0x1525B2a2093E700e17E2749536237C01fE4B4e20",
+};
 
 export const DESTINATION_CHAINS = [
   zora,
